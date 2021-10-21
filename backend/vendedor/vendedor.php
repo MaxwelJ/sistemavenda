ee1b9f23b725225583dffc0725fb8885f8b15a1b
Maxwel Jonson Martins de Melo <maxwel.martins@adagri.local>
2021-10-21 14:45:45 -0300

    Finalizado parte de vendedores

--- backend/vendedor/vendedor.php
+++ backend/vendedor/vendedor.php
@@ -4,6 +4,7 @@ require_once '../../vendor/autoload.php';
 use App\Model\Vendedor;
 
 $vendedor = new Vendedor();
+// var_dump($_GET['id']);die;
 $resultado = $vendedor->getVendedor($_GET['id']);
 
 echo json_encode($resultado);
---
+++ produtos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+require_once 'vendor/autoload.php';
+$vendedorModel = new \App\Model\Vendedor();
+$vendedores = $vendedorModel->listar();
+?>
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.min.js" integrity="sha512-Y/GIYsd+LaQm6bGysIClyez2HGCIN1yrs94wUrHoRAD5RSURkqqVQEU6mM51O90hqS80ABFTGtiDpSXd2O05nw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+    <script src="https://kit.fontawesome.com/d560921102.js" crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+    <link href="css/index.css" rel="stylesheet">
+    <script src="js/vendedor.js"></script>
+    <title>Loja Variedas</title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
